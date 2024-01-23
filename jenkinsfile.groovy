8e4fa9f35a4ec3909b4307d32499b69fd0f4faaf
AnilkumarMaguluri18 <104952076+AnilkumarMaguluri18@users.noreply.github.com>
2024-01-23 17:57:45 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,42 +2,34 @@ pipeline {
     agent any
 
     environment {
-        JENKINS_HOME = tool 'Terraform'
+        TF_HOME = tool 'Terraform'
     }
 
     stages {
         stage('Checkout') {
             steps {
-                checkout([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[url: 'https://github.com/AnilkumarMaguluri18/Jenkinsfile.git']]])
-            }
-        }
-
-        stage('Checkout Terraform') {
-            steps {
-                dir('terraform') {
-                    checkout([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[url: 'https://github.com/AnilkumarMaguluri18/terraform_file.git']]])
+                script {
+                    deleteDir()
+                    checkout scm
                 }
             }
         }
 
-        stage('Plan') {
+        stage('Terraform Init') {
             steps {
                 script {
                     dir('terraform') {
-                        withCredentials([usernamePassword(credentialsId: 'my-aws-credentials', passwordVariable: 'TF_API_TOKEN', usernameVariable: 'TF_USERNAME')]) {
-                            script {
-                                // Set Terraform environment variables
-                                env.TF_PLUGIN_CACHE_DIR = "${JENKINS_HOME}\\terraform\\plugins"
-                                env.TF_DATA_DIR = "${JENKINS_HOME}\\terraform\\data"
-                                
-                                // Initialize Terraform
-                                sh 'terraform init -input=false'
-                                
-                                // Run Terraform plan
-                                sh 'terraform plan -out=tfplan -input=false'
-                                sh 'terraform show -no-color tfplan > tfplan.txt'
-                            }
-                        }
+                        sh "${TF_HOME}/terraform init -backend-config='access_key=${TF_API_TOKEN}' -backend-config='secret_key=${TF_USERNAME}'"
+                    }
+                }
+            }
+        }
+
+        stage('Terraform Plan') {
+            steps {
+                script {
+                    dir('terraform') {
+                        sh "${TF_HOME}/terraform plan -out=tfplan"
                     }
                 }
             }
@@ -45,16 +37,17 @@ pipeline {
 
         stage('Approval') {
             steps {
-                // Your approval steps here
+                script {
+                    input 'Do you want to apply changes?'
+                }
             }
         }
 
-        stage('Apply') {
+        stage('Terraform Apply') {
             steps {
                 script {
                     dir('terraform') {
-                        // Run Terraform apply
-                        sh 'terraform apply -auto-approve tfplan'
+                        sh "${TF_HOME}/terraform apply -auto-approve tfplan"
                     }
                 }
             }
