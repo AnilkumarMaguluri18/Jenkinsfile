d23cc992427552ec80aa4f8d636e859115831a76
AnilkumarMaguluri18 <104952076+AnilkumarMaguluri18@users.noreply.github.com>
2024-01-23 17:05:24 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,7 +4,6 @@ pipeline {
     stages {
         stage('Checkout Terraform Script') {
             steps {
-                // Git clone your Terraform script repository
                 git url: 'https://github.com/AnilkumarMaguluri18/terraform_file.git', branch: 'main'
             }
         }
@@ -12,9 +11,7 @@ pipeline {
         stage('Terraform Init') {
             steps {
                 script {
-                    // Change to the directory where your Terraform files are cloned
                     dir('terraform_file') {
-                        // Run Terraform init
                         sh 'terraform init'
                     }
                 }
@@ -24,12 +21,9 @@ pipeline {
         stage('Terraform Apply') {
             steps {
                 script {
-                    // Change to the directory where your Terraform files are cloned
                     dir('terraform_file') {
-                        // Run Terraform apply
-                        withAWS(region: 'us-east-2', credentials: 'my-aws-credentials') {
-                            sh 'terraform apply -auto-approve'
-                        }
+                        // Use 'call' to invoke the appropriate command based on OS
+                        bat(script: 'terraform apply -auto-approve -var-file=vpc_aws.tf', returnStatus: true) || sh 'terraform apply -auto-approve -var-file=vpc_aws.tf'
                     }
                 }
             }
