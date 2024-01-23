36defeefa57a2f1da7027c2b713fba39739cc65d
AnilkumarMaguluri18 <104952076+AnilkumarMaguluri18@users.noreply.github.com>
2024-01-23 17:46:26 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,46 +1,56 @@
-Pipeline {
+pipeline {
+    parameters {
+        booleanParam(name: 'autoApprove', defaultValue: false, description: 'Automatically run apply generating plan?')
+    }
 
-parameters {
-booleanparam(name: 'autoApprove', defaultValue: false, description:'Automatically run apply generating plan?')
-}
+    agent any
 
-agent any
-stages {
-stage('checkout') {
-steps {
-script {
-dir('terraform')
-{
-git "https://github.com/AnilkumarMaguluri18/terraform_file.git"
-}
-}
-}
-}
-Stage('plan') {
-steps {
-sh'pwd;cd terraform/ ; terraform init'
-sh'pwd;cd terraform/ ; terraform plan -out tfplan'
-sh'pwd;cd terraform/ ; terraform show -no-colour tfplan > tfplan.txt'
-}
-}
-stage('Aproval') {
-when {
-not {
-equals expected: true, actual: params.autoApprove
-}
-}
-steps {
-script {
-def paln = readfile 'terraform/tfplan.txt'
-input message: "Do you want to apply the plan?",
-parameters: [text(name: 'plan', description: 'please review the plan', defaultValue: plan)]
-}
-}
-}
-stage('Apply') {
-step {
-sh "pwd;cd terraform/ ; terraform apply -input=false tfplan"
-}
-}
-}
+    stages {
+        stage('checkout') {
+            steps {
+                script {
+                    dir('terraform') {
+                        git "https://github.com/AnilkumarMaguluri18/terraform_file.git"
+                    }
+                }
+            }
+        }
+
+        stage('plan') {
+            steps {
+                script {
+                    dir('terraform') {
+                        sh 'pwd; terraform init'
+                        sh 'pwd; terraform plan -out tfplan'
+                        sh 'pwd; terraform show -no-color tfplan > tfplan.txt'
+                    }
+                }
+            }
+        }
+
+        stage('Approval') {
+            when {
+                not {
+                    expression { params.autoApprove }
+                }
+            }
+            steps {
+                script {
+                    def plan = readFile 'terraform/tfplan.txt'
+                    input message: 'Do you want to apply the plan?',
+                        parameters: [text(name: 'plan', description: 'Please review the plan', defaultValue: plan)]
+                }
+            }
+        }
+
+        stage('Apply') {
+            steps {
+                script {
+                    dir('terraform') {
+                        sh 'pwd; terraform apply -input=false tfplan'
+                    }
+                }
+            }
+        }
+    }
 }
