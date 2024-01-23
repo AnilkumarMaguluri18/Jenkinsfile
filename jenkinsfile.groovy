45e42cf7633b0a1a58df3ad9c2e80bfb4bc9f9f6
AnilkumarMaguluri18 <104952076+AnilkumarMaguluri18@users.noreply.github.com>
2024-01-23 18:04:05 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,26 +1,27 @@
 pipeline {
     agent any
 
+    tools {
+        terraform 'Terraform' // Make sure this matches the tool name configured in Jenkins
+    }
+
     environment {
         TF_HOME = tool 'Terraform'
     }
 
     stages {
-        stage('Checkout') {
+        stage('Checkout SCM') {
             steps {
-                script {
-                    deleteDir()
-                    checkout scm
-                }
+                // Checkout your source code from SCM (Git in this case)
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[url: 'https://github.com/AnilkumarMaguluri18/Jenkinsfile.git']]])
             }
         }
 
         stage('Terraform Init') {
             steps {
                 script {
-                    dir('terraform') {
-                        sh "${TF_HOME}/terraform init -backend-config='access_key=${TF_API_TOKEN}' -backend-config='secret_key=${TF_USERNAME}'"
-                    }
+                    // Run Terraform init
+                    sh "${TF_HOME}/terraform init -input=false"
                 }
             }
         }
@@ -28,17 +29,8 @@ pipeline {
         stage('Terraform Plan') {
             steps {
                 script {
-                    dir('terraform') {
-                        sh "${TF_HOME}/terraform plan -out=tfplan"
-                    }
-                }
-            }
-        }
-
-        stage('Approval') {
-            steps {
-                script {
-                    input 'Do you want to apply changes?'
+                    // Run Terraform plan
+                    sh "${TF_HOME}/terraform plan -out=tfplan -input=false"
                 }
             }
         }
@@ -46,9 +38,17 @@ pipeline {
         stage('Terraform Apply') {
             steps {
                 script {
-                    dir('terraform') {
-                        sh "${TF_HOME}/terraform apply -auto-approve tfplan"
-                    }
+                    // Run Terraform apply
+                    sh "${TF_HOME}/terraform apply -input=false -auto-approve tfplan"
+                }
+            }
+        }
+
+        stage('Terraform Destroy') {
+            steps {
+                script {
+                    // Run Terraform destroy
+                    sh "${TF_HOME}/terraform destroy -input=false -auto-approve"
                 }
             }
         }
