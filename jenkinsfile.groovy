ba35b4e733f12a3b7e4e17465838e630777edcf4
AnilkumarMaguluri18 <104952076+AnilkumarMaguluri18@users.noreply.github.com>
2024-01-26 14:53:10 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,8 +2,8 @@ pipeline {
     agent any
 
     environment {
-        AWS_ACCESS_KEY_ID     = credentials('Access Key ID')
-        AWS_SECRET_ACCESS_KEY = credentials('Secret Access Key')
+        AWS_ACCESS_KEY_ID     = credentials('my-aws-credentials')
+        AWS_SECRET_ACCESS_KEY = credentials('my-aws-credentials')
     }
 
     stages {
